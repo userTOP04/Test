e096a8016e7dd26ea2301539241c51f70b7e4587
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2024-03-05 17:45:58 +0300

    Add files via upload

--- my_test21.py
+++ my_test21.py
@@ -1,3 +1,5 @@
+from random import shuffle
+import os  
 """
 
 Карта:
@@ -39,36 +41,85 @@
 
 
 """
-suits = ['бубны', 'черви', 'пики', 'крести']
-names = ['6', '7', '8', '9', '10', 'валет', 'дама', 'король', 'туз']
-tens = []
+def get_dect() -> list[dict]:
+    suits = ['бубны', 'черви', 'пики', 'крести']
+    names = ['6', '7', '8', '9', '10', 'валет', 'дама', 'король', 'туз']
+    deck = []
 
-deck = []
+    for suit in suits:
+        for name in names:
+            if name in ('валет', 'дама', 'король'):
+                value = 10
+            elif name == 'туз':
+                value = 11
+            else:
+                value = int(name)
+            Cart = {
+                'имя': name,
+                'цена': value,
+                'масть': suit
+            }
+            deck.append(Cart)
 
-for suit in suits:
-    for name in names:
-        if name in ('валет' 'дама', 'король'):
-            value = 10
-        elif name == 'туз':
-            value = 11
+    return deck
+
+
+
+def get_players() -> list[dict]:
+    player_1 = {
+        'человек': True,
+        'имя': 'Вася',
+        'карты': [],
+        'счет': 10
+    }
+    player_2 = {
+        'человек': True,
+        'имя': 'Ася',
+        'карты': [],
+        'счет': 10
+    }
+    return [player_1, player_2]
+
+
+def deal_cards(num: int) -> None:
+    for player in players:
+        for i in range(num):
+            player['карты'].append(deck.pop(-1))
+
+
+def show_cards() -> None:
+    for cart in player['карты']:
+            print(cart['имя'], cart['масть'])
+
+
+deck = get_dect()
+shuffle(deck)
+players = get_players()
+deal_cards(2)
+
+
+for player in players:
+    for i in range(1):
+        player['карты'].append(deck.pop(-1))
+
+for player in players:
+    while True:
+        os.system('cls') 
+        show_cards()
+        player_option = input('Взять картe? y/n: ')
+        if player_option == 'y':
+            player['карты'].append(deck.pop(-1))
         else:
-            value = int(name)
-        Cart = {
-        'вес': name,
-        'цена': value,
-        'масть': suit
-        }
-        deck.append(Cart)
+            break
 
-print('Всего в колде', len(deck))
-for cart in deck:
-    print(cart)
+os.system('cls') 
+for player in players: 
+    show_cards()
+    total = 0
+    for cart in player['карты']:
+        total += cart['цена']
+    print(f'У {player["имя"]} {total} очков')
     print('-' * 20)
 
-"""
-Cart = {
-    'вес': '10',
-    'цена': 10,
-    'масть': 'пик',
-}
-"""
+# У каждого игрока нужно хранить сумму очков
+
